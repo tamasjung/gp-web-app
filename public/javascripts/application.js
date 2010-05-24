bd6c1924ce597b6b22004e5344d4a710a7330c3d
Tamas Jung <tamasjung@gmail.com>
2010-05-24 10:41:32 +0200

    daily commit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function builder(container_id){
+  $(container_id);
+}
+function text_field(name, type_obj){
+  return new Element('a', { 'class': 'foo', href: '/foo.html' }).update("heheheheheh");
+} 
