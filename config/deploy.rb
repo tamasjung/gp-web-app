be8ab86ba6ce7363aff63d49086d709fd6489836
Tamas Jung <tamasjung@gmail.com>
2010-05-25 21:05:38 +0200

    rails update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,3 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
-
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
@@ -20,3 +10,41 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+
+# be sure to change these
+set :user, 'griduser' 
+set :domain, '10.0.0.35' 
+set :application, 'gp-web-app'
+# file paths
+#set :repository, "#{user}@#{git_repo_host}:git/#{application}.git" 
+set :repository, "ssh://git@interface11.repositoryhosting.com/interface11/gp-web-app.git"
+set :deploy_to, "/home/#{user}/#{application}"
+
+role :app, domain 
+role :web, domain 
+role :db, domain, :primary => true
+
+# you might need to set this if you aren't seeing password prompts # default_run_options[:pty] = true
+# As Capistrano executes in a non-interactive mode and therefore doesn't cause # any of your shell profile scripts to be run, the following might be needed # if (for example) you have locally installed gems or applications. Note: # this needs to contain the full values for the variables set, not simply
+# the deltas. # default_environment['PATH']='<your paths>:/usr/local/bin:/usr/bin:/bin' # default_environment['GEM_PATH']='<your paths>:/usr/lib/ruby/gems/1.8'
+# miscellaneous options
+set :deploy_via, :remote_cache 
+
+set :scm, 'git' 
+set :branch, 'master' 
+set :scm_verbose, true
+set :use_sudo, false
+# task which causes Passenger to initiate a restart
+namespace :deploy do 
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end 
+end
+# optional task to reconfigure databases
+after "deploy:update_code", :configure_database 
+desc "copy database.yml into the current release path" 
+task :configure_database, :roles => :app do
+  db_config = "#{deploy_to}/localconfig/database.yml"
+  run "cp #{db_config} #{release_path}/config/database.yml"
+end
