437c3ccb5c92056340bc56325cbbd366bf5e68ee
Tamas Jung <tamasjung@gmail.com>
2010-09-10 13:15:58 +0200

    meep ser, deser is ready

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+
+
+
 function confirm_dialog(title, question, onOkFunction, onCancelFunction){
   var window;
   var destroyFunction = function(){
