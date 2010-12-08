43abd859fe8cf6d7bdefff7e7110d2f7463810f7
Tamas Jung <tamasjung@gmail.com>
2010-12-08 18:47:28 +0100

    remote user feature is not finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,25 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_session, :current_user
   before_filter :require_user
 
-
+  def remote_user
+    result = nil
+    if request.env['HTTPS']
+      remote_user = request.env['REMOTE_USER']
+      if remote_user
+        person = Person.find_by_remote_id remote_user
+        unless person
+          
+        end
+        result = person
+      end
+    end
+    result        
+  end
 
   def current_user
     return @current_user if defined?(@current_user)
+
+        
     @current_user = current_user_session && current_user_session.person
   end
   
