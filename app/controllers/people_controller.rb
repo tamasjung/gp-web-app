54e391a7e94ec08fccf5b7d77e2bf236423537e0
Tamas Jung <tamasjung@gmail.com>
2010-12-16 14:40:17 +0100

    people select

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,7 +6,31 @@ class PeopleController < ApplicationController
   skip_before_filter :require_user, :only => [:new, :create]
   skip_before_filter :require_nickname, :only => [:edit, :update]
   
-
+  def select
+    search_string = params[:person_search]
+    options = {:page => params[:page], :order => params[:orders]}
+    
+    if search_string
+      begin
+        parser = FilterParser.new(:person, [:nickname])
+        options.merge!(parser.parse(search_string)) 
+      end
+    end
+    @people = Person.paginate options
+    respond_to do |format|
+      format.js do
+        render :update do |page|
+          page.replace_html 'person_select', :partial => "select"
+        end
+      end
+    end
+  end
+  
+  def search_autocomplete
+    value = params[:value]
+    @results = FilterAutoComplete.new(Person, [:nickname]).get_results(value)
+    render :partial => '/layouts/search_autocomplete'
+  end
 
   def new
     @person = Person.new
